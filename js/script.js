86aa8ac22062843f4a4abbe85030e26800c2d2d9
CamillaKleymen <camillakleymen@outlook.com>
2024-12-21 17:10:16 +0500

    main page done

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
       dots: false,
       infinite: true,
       arrows: true,
-      speed: 300,
+      autoplay: true,
+      autoplaySpeed: 0,
+      speed: 7000,
       slidesToShow: 3,
       slidesToScroll: 1,
       prevArrow: $('.slider-prev'),
@@ -33,3 +35,96 @@ $(document).ready(function() {
       ]
     });
   });
+
+
+  $('.slider-hero').slick({
+    dots: false,
+    infinite: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 7000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.slider-prev'),
+    nextArrow: $('.slider-next'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+  $('.slider-brand-left').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 5000,
+    cssEase: 'linear',
+    arrows: false,
+    rtl: false 
+  });
+
+
+  $('.slider-brand-right').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 5000,
+    cssEase: 'linear',
+    arrows: false,
+    rtl: false 
+  });
+
+
+ 
+
+  $(document).ready(function () {
+    $('.slider-bestsellers').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: $('.slider-prev'),
+        nextArrow: $('.slider-next'),
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+
+
+
+
+
+
+$(document).ready(function() {
+    AOS.init();
+});
